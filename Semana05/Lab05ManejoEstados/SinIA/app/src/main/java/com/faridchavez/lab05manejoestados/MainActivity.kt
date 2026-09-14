85a902bdc259323f35fee8b6c051c97a93241fd4
faridchz16 <faridchavezcampos@gmail.com>
2026-09-13 20:15:18 -0500

    Cree el proyecto de manejo de estados

---
+++ Semana05/Lab05ManejoEstados/SinIA/app/src/main/java/com/faridchavez/lab05manejoestados/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.faridchavez.lab05manejoestados
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.faridchavez.lab05manejoestados.ui.theme.Lab05ManejoEstadosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab05ManejoEstadosTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab05ManejoEstadosTheme {
+        Greeting("Android")
+    }
+}
