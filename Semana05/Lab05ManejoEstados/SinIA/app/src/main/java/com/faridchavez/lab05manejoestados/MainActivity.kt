744d2980cbf06fa6c9ba3afe76fa6318b5be2912
faridchz16 <faridchavezcampos@gmail.com>
2026-09-13 20:26:00 -0500

    Implemente el contador con manejo de estados

--- Semana05/Lab05ManejoEstados/SinIA/app/src/main/java/com/faridchavez/lab05manejoestados/MainActivity.kt
+++ Semana05/Lab05ManejoEstados/SinIA/app/src/main/java/com/faridchavez/lab05manejoestados/MainActivity.kt
@@ -3,45 +3,81 @@ package com.faridchavez.lab05manejoestados
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.faridchavez.lab05manejoestados.ui.theme.Lab05ManejoEstadosTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             Lab05ManejoEstadosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ContadorConRemember()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ContadorRoto() {
+
+    var contador = 0
+
+    Column {
+        Text("Contador: $contador")
+
+        Button(
+            onClick = {
+                contador++
+            }
+        ) {
+            Text("Incrementar")
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Lab05ManejoEstadosTheme {
-        Greeting("Android")
+fun ContadorConRemember() {
+
+    var contador by remember {
+        mutableStateOf(0)
+    }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Contador: $contador",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(
+            modifier = Modifier.height(8.dp)
+        )
+
+        Button(
+            onClick = {
+                contador++
+            }
+        ) {
+            Text("Incrementar")
+        }
     }
 }
