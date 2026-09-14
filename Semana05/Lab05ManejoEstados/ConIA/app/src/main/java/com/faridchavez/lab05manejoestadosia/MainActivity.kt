1fb579a68af4e129722bbf3b0d649131254fba0f
faridchz16 <faridchavezcampos@gmail.com>
2026-09-13 21:10:58 -0500

    Agregue el campo para registrar nuevas tareas

--- Semana05/Lab05ManejoEstados/ConIA/app/src/main/java/com/faridchavez/lab05manejoestadosia/MainActivity.kt
+++ Semana05/Lab05ManejoEstados/ConIA/app/src/main/java/com/faridchavez/lab05manejoestadosia/MainActivity.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.faridchavez.lab05manejoestadosia.ui.theme.Lab05ManejoEstadosIATheme
@@ -29,6 +38,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun PantallaTareas() {
 
+    var textoTarea by remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -38,6 +51,36 @@ fun PantallaTareas() {
         Text(
             text = "Lista de tareas - Tecsup",
             style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(
+            modifier = Modifier.height(16.dp)
+        )
+
+        OutlinedTextField(
+            value = textoTarea,
+            onValueChange = {
+                textoTarea = it
+            },
+            label = {
+                Text("Ingrese una tarea")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(
+            modifier = Modifier.height(8.dp)
+        )
+
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Agregar tarea")
+        }
+
+        Spacer(
+            modifier = Modifier.height(16.dp)
         )
 
         Text(
